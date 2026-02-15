cf2e938983a1adbe8acbd2361dee93fea74fc8d8
eugene <potaraev@gmail.com>
2026-02-15 15:30:36 +0300

    KS-1-3 - вывод в консоль года и времени первого полета Гагарина в космос

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,28 @@
 package org.example.lesson_1
 
 fun main() {
-    println ("Тестовое решение к уроку 1 задаче 3")
+    // год и время первого взлета Ю. Гагарина в космос
+    val  year: Short = 1961         //год не изменяется
+    var hour: Byte = 9              //час изменимо
+    var minute: Byte = 7            //минуты изменимо
+
+    //вывод в консоль года и времени взлета
+    println()
+    print ("Год полета - ")
+    println(year)
+    print("Час взлета - ")
+    println(hour)
+    print("Минуты взлета - ")
+    println(minute)
+
+    //изменение времени на время посадки
+    hour = 10
+    minute = 55
+
+    //вывод времени посадки в одной строке
+    println()
+    print("Время посадки - ")
+    print(hour)
+    print(":")
+    println(minute)
 }
