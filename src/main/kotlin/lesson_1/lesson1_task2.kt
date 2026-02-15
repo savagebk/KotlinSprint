2bf10a60eb70709452e11fe6714dc848cce82b4a
eugene <potaraev@gmail.com>
2026-02-15 13:33:10 +0300

    KS-1-2 Добавил подсчет работников и вывод их числа в консоль

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,5 +1,24 @@
 package org.example.lesson_1
 
 fun main() {
-    println ("Тестовое решение к уроку 1 задаче 2")
+    var orderQuantity: Int = 75                         //количество заказов
+    val thanksAlert: String = "Спасибо за покупку"      //текст благодарности за покупку
+    var employeesCount: Int = 2000                    //количество работников
+
+    //вывод в консоль значений количества заказов и содержимого благодарности за покупку
+    println()
+    print("Количество заказов - ")
+    println(orderQuantity)
+    print("Текст благодарности за покупку - ")
+    println(thanksAlert)
+
+    //вывод в консоль количества работников
+    //print("Количество работников - ")
+    //println(employeesCount)
+
+    //количество работников изменяется
+    employeesCount -= 1
+    //вывод в консоль количества работников
+    print("Количество работников - ")
+    println(employeesCount)
 }
