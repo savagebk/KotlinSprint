10d6acf37305a90a315d4e46d4ff706fc8132a00
eugene <potaraev@gmail.com>
2026-02-17 18:21:56 +0300

    KS-1-5 - убраны магические числа и использован String.format()

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,16 +2,23 @@ package org.example.lesson_1
 
 fun main() {
 
-    val totalSeconds: Short = 6480                                                  //количество секунд, которое Гагарин провел в космосе
-    val hours: Byte = (totalSeconds / 3600).toByte()                                //количество целых часов, которое Гагарин провел в космосе
-    val minutes: Byte = ((totalSeconds % 3600) / 60).toByte()                       //количество минут вдобавок к часам, которое Гагарин провел в космосе
-    val seconds: Byte = (((totalSeconds % 3600) % 60) % 60).toByte()                //количество секунд вдобавок к часам и минутам, которое Гагарин провел в космосе
+    val totalSeconds = 6480                                                         //количество секунд, которое Гагарин провел в космосе
+    val secInHour = 3600                                                            //секунд в часу
+    val secInMinute = 60                                                            //секунд в минуте
+    val hours = (totalSeconds / secInHour)                                          //количество целых часов, которое Гагарин провел в космосе
+    val minutes = ((totalSeconds % secInHour) / secInMinute)                        //количество целых минут вдобавок к часам, которое Гагарин провел в космосе
+    val seconds = (((totalSeconds % secInHour) % secInMinute) % secInMinute)        //количество секунд вдобавок к часам и минутам, которое Гагарин провел в космосе
+
+    //форматирование времени в формат hh:mm:ss
+    val formattedHours = String.format("%02d", hours)
+    val formattedMinutes = String.format("%02d", minutes)
+    val formattedSeconds = String.format("%02d", seconds)
 
     //вывод времени, проведенного в космосе Гагариным в одну строку с конвертированием в String и добавлением лидирующих нолей для приведения к формату hh:mm:ss
     print("Время, проведенное в космосе Гагариным - ")
-    print(hours.toString().padStart(2, '0'))
+    print(formattedHours)
     print(":")
-    print(minutes.toString().padStart(2, '0'))
+    print(formattedMinutes)
     print(":")
-    println(seconds.toString().padStart(2, '0'))
+    println(formattedSeconds)
 }
