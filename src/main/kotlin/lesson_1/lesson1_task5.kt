c406a0fb47e1c2d3599f5fe13a84d89e246c632f
eugene <potaraev@gmail.com>
2026-02-18 19:34:09 +0300

    KS-1-5 Расчет времени проведенного в космосе Гагариным

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,24 +1,17 @@
 package org.example.lesson_1
 
+const val SECONDS_IN_HOUR = 3600                                                                    //секунд в часу
+const val SECONDS_IN_MINUTE = 60                                                                    //секунд в минуте
+
 fun main() {
 
-    val totalSeconds = 6480                                                         //количество секунд, которое Гагарин провел в космосе
-    val secInHour = 3600                                                            //секунд в часу
-    val secInMinute = 60                                                            //секунд в минуте
-    val hours = (totalSeconds / secInHour)                                          //количество целых часов, которое Гагарин провел в космосе
-    val minutes = ((totalSeconds % secInHour) / secInMinute)                        //количество целых минут вдобавок к часам, которое Гагарин провел в космосе
-    val seconds = (((totalSeconds % secInHour) % secInMinute) % secInMinute)        //количество секунд вдобавок к часам и минутам, которое Гагарин провел в космосе
+    val totalSeconds = 6480                                                                         //количество секунд, которое Гагарин провел в космосе
 
-    //форматирование времени в формат hh:mm:ss
-    val formattedHours = String.format("%02d", hours)
-    val formattedMinutes = String.format("%02d", minutes)
-    val formattedSeconds = String.format("%02d", seconds)
+    val hours = (totalSeconds / SECONDS_IN_HOUR)                                                    //количество целых часов, которое Гагарин провел в космосе
+    val minutes = ((totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE)                            //количество целых минут вдобавок к часам, которое Гагарин провел в космосе
+    val seconds = (((totalSeconds % SECONDS_IN_HOUR) % SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE)      //количество секунд вдобавок к часам и минутам, которое Гагарин провел в космосе
 
-    //вывод времени, проведенного в космосе Гагариным в одну строку с конвертированием в String и добавлением лидирующих нолей для приведения к формату hh:mm:ss
-    print("Время, проведенное в космосе Гагариным - ")
-    print(formattedHours)
-    print(":")
-    print(formattedMinutes)
-    print(":")
-    println(formattedSeconds)
+        //вывод времени, проведенного в космосе Гагариным в одну строку с конвертированием в String и добавлением лидирующих нолей для приведения к формату hh:mm:ss
+    println("Время, проведенное в космосе Гагариным - " + "%02d:%02d:%02d".format(hours, minutes, seconds))
+
 }
