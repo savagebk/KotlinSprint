afb7a462fb5711be7af6bcd67baef2b32865087c
eugene <potaraev@gmail.com>
2026-02-18 19:50:35 +0300

    KS-2-3 Расчет времени прибытия поезда

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,27 +1,18 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60                                                                      //минут в часу
+
 fun main() {
     val startHour = 9                                                                               //час отправления поезда
     val startMinute = 39                                                                            //минуты отправления поезда
     val duration = 457                                                                              //время в пути, минут
 
-    val minutesInHour = 60                                                                          //минут в часу
-
-    var overallStartMinutes = startHour * minutesInHour + startMinute                               //время отправления в минутах, прошедших после полуночи текущих суток
+    var overallStartMinutes = startHour * MINUTES_IN_HOUR + startMinute                             //время отправления в минутах, прошедших после полуночи текущих суток
     var finishOverallMinutes = overallStartMinutes + duration                                       //время прибытия в минутах, прошедших после полуночи текущих суток
 
-    var finishHours = finishOverallMinutes / minutesInHour                                          //час прибытия
-    var finishMinutes = finishOverallMinutes % minutesInHour                                        //минуты прибытия
+    var finishHours = finishOverallMinutes / MINUTES_IN_HOUR                                        //час прибытия
+    var finishMinutes = finishOverallMinutes % MINUTES_IN_HOUR                                      //минуты прибытия
 
-    //форматирование времени прибытия поезда в формат hh:mm
-    var formattedFinishHour = String.format("%02d", finishHours)
-    var formattedFinishMinutes = String.format("%02d", finishMinutes)
-
-    //вывод времени прибытия поезда
-    println()
-    print("Время прибытия поезда - ")
-    print(formattedFinishHour)
-    print(":")
-    println(formattedFinishMinutes)
+    println("Время прибытия поезда - " + "%02d:%02d".format(finishHours, finishMinutes))    //вывод времени прибытия поезда в формате hh:mm
 
 }
