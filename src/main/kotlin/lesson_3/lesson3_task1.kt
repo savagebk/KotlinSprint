1926ea00f4513837e76f4136ea0377311db231b0
eugene <potaraev@gmail.com>
2026-02-19 21:48:45 +0300

    KS-3-1 Вывод приветствия в зависимости от времени

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,9 +2,9 @@ package org.example.lesson_3
 
 fun main() {
 
-    val helloDay = "Приветствую"                                        //текст дневного приветствия
-    val helloEvening = "Добрый вечер"                                   //текст вечернего приветствия
-    var userName = "Алексей"                                            //имя пользователя
+    val helloDay = "Приветствую"
+    val helloEvening = "Добрый вечер"
+    var userName = "Алексей"
 
     var helloText = "$helloDay, $userName!"
     println(helloText)
