07d5e80500a62b6c8b96cbea2601463adf337cfc
eugene <potaraev@gmail.com>
2026-02-20 10:50:09 +0300

    KS-3-3 Вывод таблицы умножения

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val multiplicand = 8
+    println ("""
+        $multiplicand x 1 = ${multiplicand * 1}
+        $multiplicand x 2 = ${multiplicand * 2}
+        $multiplicand x 3 = ${multiplicand * 3}
+        $multiplicand x 4 = ${multiplicand * 4}
+        $multiplicand x 5 = ${multiplicand * 5}
+        $multiplicand x 6 = ${multiplicand * 6}
+        $multiplicand x 7 = ${multiplicand * 7}
+        $multiplicand x 8 = ${multiplicand * 8}
+        $multiplicand x 9 = ${multiplicand * 9}""".trimIndent())
+
+}
