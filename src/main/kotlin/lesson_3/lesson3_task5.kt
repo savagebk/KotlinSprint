af4d518e04f06e91d721f2eec1c91cdeeb5ddff6
eugene <potaraev@gmail.com>
2026-02-20 13:42:07 +0300

    KS-3-5 Парсинг хода в шахматах

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,8 +2,14 @@ package org.example.lesson_3
 
 fun main() {
 
-    var currentMove = "D2-D4;0"
+    var currentMoveData = "D2-D4;0"
 
+    var from = currentMoveData.substring(0..1)
+    var to = currentMoveData.substring(3..4)
+    var move = currentMoveData.substring(6..6)
 
+    println ("Начальное положение фигуры: $from")
+    println("Конечное положение фигуры: $to")
+    println("Номер хода: $move")
 
 }
