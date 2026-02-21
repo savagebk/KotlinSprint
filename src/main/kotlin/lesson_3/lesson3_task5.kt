55194d64f256a67eb66c376cd1f4120b350b3411
eugene <potaraev@gmail.com>
2026-02-21 07:40:11 +0300

    KS-3-5 Парсинг хода в шахматах

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,10 +3,11 @@ package org.example.lesson_3
 fun main() {
 
     var currentMoveData = "D2-D4;0"
+    var splitted = currentMoveData.split("-", ";")
 
-    var from = currentMoveData.substring(0..1)
-    var to = currentMoveData.substring(3..4)
-    var move = currentMoveData.substring(6..6)
+    var from = splitted [0]
+    var to = splitted [1]
+    var move = splitted [2]
 
     println ("Начальное положение фигуры: $from")
     println("Конечное положение фигуры: $to")
