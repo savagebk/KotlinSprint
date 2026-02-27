ac103168ef8a1fe65622f1e060b44c4bb162f2cb
eugene <potaraev@gmail.com>
2026-02-27 11:01:30 +0300

    KS-5-5.6 - расчет индекса масы тела

---
+++ src/main/kotlin/lesson_5/lesson5_task5.6.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_5
+
+import kotlin.math.roundToInt
+
+const val MIN_BMI = 18.5f
+const val NORMAL_BMI = 25.0f
+const val MAX_BMI = 30.0f
+const val CM_IN_M = 100             //сантиметров в метре
+
+fun main() {
+
+    println ("Расчет индекса массы тела")
+    print ("Введите ваш рост в сантиметрах: ")
+    val height = (readln().toFloat()) / CM_IN_M
+    print ("Введите вашу массу в килограммах: ")
+    val weight = readln().toFloat()
+
+    val bmi: Float = weight / (height * height)
+    print("Индекс массы тела равен ${(bmi*100).roundToInt()/100.0} - ")
+
+    when {
+        bmi < MIN_BMI -> println ("недостаточная маса тела.")
+        bmi >= MIN_BMI && bmi < NORMAL_BMI -> println ("нормальная маса тела.")
+        bmi >= NORMAL_BMI && bmi < MAX_BMI -> println ("нзбыточная маса тела.")
+    else -> println("ожирение.")
+        }
+}
