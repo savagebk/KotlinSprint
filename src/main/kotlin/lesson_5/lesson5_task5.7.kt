7dc6515ea4dbd18e5b03389e0ee223bc5c567f47
eugene <potaraev@gmail.com>
2026-02-27 11:42:51 +0300

    KS-5-5.7 - Расчет общего расхода и стоимости топлива

---
+++ src/main/kotlin/lesson_5/lesson5_task5.7.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_5
+
+import kotlin.math.roundToInt
+
+const val RATIONED_DISTANCE = 100
+
+fun main() {
+
+    print("Введите расстояние поездки (в километрах): ")
+    val distance = readln().toFloat()
+    print("Введите расход топлива на $RATIONED_DISTANCE км (в литрах): ")
+    val consumtion = readln().toFloat()
+    print("Введите текущую цену за литр топлива: ")
+    val price = readln().toFloat()
+
+    val gasVolume : Float = consumtion * distance / RATIONED_DISTANCE
+    val gasValue : Float = gasVolume * price
+
+    println("Общее количество необходимого топлива ${(gasVolume*100).roundToInt()/100f} л.")
+    println("Стоимость поездки ${(gasValue*100).roundToInt()/100f}")
+}
