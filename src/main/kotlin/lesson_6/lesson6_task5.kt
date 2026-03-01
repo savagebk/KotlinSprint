9eec0b5d00aa18bf07172551ad4d5f9ecaf0fd2d
eugene <potaraev@gmail.com>
2026-03-01 14:49:02 +0300

    KS-6-5 Авторизация (3 попытки)

---
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_6
+
+import kotlin.random.Random
+
+const val MAX_TRIES = 3
+
+fun main() {
+
+    var number1 : Int
+    var number2 : Int
+    var triesLeft = MAX_TRIES
+
+    println("Здравствуйте!")
+    println("Для подтверждения того, что вы человек, введите результат выражения:")
+
+    while (triesLeft-- > 0) {
+        number1 = Random.nextInt(1, 9)
+        number2 = Random.nextInt(1, 9)
+        print("$number1 + $number2 = ")
+        if ((number1 + number2) == readln().toInt()) {
+            println("Добро пожаловать!")
+            break
+        } else {
+            if (triesLeft == 0) {
+                println("Доступ запрещен")
+            } else {
+                println("Неверно. Попробуйте еще раз.")
+            }
+        }
+    }
+
+}
