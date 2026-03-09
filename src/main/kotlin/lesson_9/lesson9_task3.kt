4963d647e1afe5c35f515d698bff172203f72519
eugene <potaraev@gmail.com>
2026-03-09 15:52:25 +0300

    KS-9-3 Расчет количества ингредиентов на нужное количество порций

---
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_9
+
+fun main() {
+
+    val quantity = listOf(2, 50, 15)
+    println("Какое количество порций необходимо: ")
+    val portions = readln().toInt()
+    val totalQty = quantity.map { it * 2 }
+    println("На $portions порций вам понадобится: Яиц – ${totalQty[0]}, молока – ${totalQty[1]}, сливочного масла – ${totalQty[2]}")
+}
