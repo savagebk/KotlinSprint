aaaff314eac93ae7dd08bd485c135a728edc83ee
eugene <potaraev@gmail.com>
2026-03-09 12:37:10 +0300

    KS-8-3 Поиск ингредиента в рецепте без цикла

---
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_8
+
+fun main() {
+
+    val ingredients = arrayOf("пельмени", "вода", "соль")
+    print("Какой ингредиент найти: ")
+    val askedIngredient = readln().lowercase()
+
+    if (ingredients.contains(askedIngredient)) {
+        println("Ингредиент \"$askedIngredient\" в рецепте есть")
+        return
+        }
+
+    println("Такого ингредиента в рецепте нет")
+}
