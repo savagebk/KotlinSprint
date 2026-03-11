a132e12be4b5260b456e08a9b56d4f9e4669c72c
eugene <potaraev@gmail.com>
2026-03-11 14:35:00 +0300

    KS-7-5 Генератор паролей

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,20 +1,52 @@
 package org.example.lesson_7
 
 const val MIN_PASSWORD_LENGTH = 6
+const val TYPES_OF_SYMBOLS = 3
+
 
 fun main() {
 
-    print("Введите длину пароля (минимум 6 символов): ")
-    var passwordLenght = readln().toInt()
-    if (passwordLenght < MIN_PASSWORD_LENGTH) {
-        passwordLenght = 6
+    val numbers = '0'..'9'
+    val smallLetters = 'a'..'z'
+    val largeLetters = 'A'..'Z'
+
+    print("Введите длину пароля (минимум $MIN_PASSWORD_LENGTH символов): ")
+    var passwordLength = readln().toInt()
+    if (passwordLength < MIN_PASSWORD_LENGTH) {
+        passwordLength = MIN_PASSWORD_LENGTH
         println("Длина пароля увеличена до $MIN_PASSWORD_LENGTH символов")
     }
-    val symbols = (0..9).toList() + ('a'..'z').toList() + ('A'..'Z').toList()
-    var password = ""
+    val symbols = numbers + largeLetters + smallLetters
+    val password = MutableList(passwordLength) { ' ' }
 
-    for (i in 1 .. passwordLenght) {
-        password = password + symbols.random()
+    val preUsedPositions = mutableListOf<Int>()
+    var currentIndex: Int
+
+    //Случайным образом назначаем позиции в пароле, в которые будут вставлены символы
+    //разных "обязательных" типов
+    for (i in 1..TYPES_OF_SYMBOLS) {
+        currentIndex = (0 until passwordLength).random()
+        while (preUsedPositions.contains(currentIndex)) {
+            currentIndex = (0 until passwordLength).random()
+        }
+        preUsedPositions.add(currentIndex)
+    }
+
+    //Заполняем ранее выбранные случайные позиции случайными символами разных типов
+    for (i in 0 until TYPES_OF_SYMBOLS) {
+        when {
+            i == 0 -> password.set(preUsedPositions[i], numbers.random())
+            i == 1 -> password.set(preUsedPositions[i], largeLetters.random())
+            i == 2 -> password.set(preUsedPositions[i], smallLetters.random())
+        }
+    }
+
+    for (i in password.indices) {
+        if (preUsedPositions.contains(i)) {
+            continue
+        } else {
+            password.set(i, symbols.random())
+        }
     }
 
     println("Ваш пароль: " + password)
