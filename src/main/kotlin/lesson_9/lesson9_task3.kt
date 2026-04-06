3cc18b0ea280393177c1362da290c71814b20605
eugene <potaraev@gmail.com>
2026-04-07 00:11:42 +0300

    wef

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -5,6 +5,6 @@ fun main() {
     val quantity = listOf(2, 50, 15)
     println("Какое количество порций необходимо: ")
     val portions = readln().toInt()
-    val totalQty = quantity.map { it * 2 }
+    val totalQty = quantity.map { it * portions }
     println("На $portions порций вам понадобится: Яиц – ${totalQty[0]}, молока – ${totalQty[1]}, сливочного масла – ${totalQty[2]}")
 }
